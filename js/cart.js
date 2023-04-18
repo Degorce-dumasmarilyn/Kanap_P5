f95e0774a14e7379bf34eecba05232ad77e6e25a
Degorce-dumasmarilyn <113439653+Degorce-dumasmarilyn@users.noreply.github.com>
2023-04-18 20:11:20 +0200

    cart.js

--- js/cart.js
+++ js/cart.js
@@ -10,23 +10,40 @@ cart.forEach((item) => displayItem(item))
 //price: 1999
 //quantity: 1
 
+
 function retrieveItemsFromCache() {
-   const numberOfItems = localStorage.length 
-   for(let i = 0;i <= numberOfItems;i++) {
-        const item = localStorage.getItem(localStorage.key(i))
-        const itemObject = JSON.parse(item)
-        cart.push(itemObject )
-    }
+  const numberOfItems = localStorage.length 
+  for (let i = 0; i <= numberOfItems; i++) {
+    const item = localStorage.getItem(localStorage.key(i))
+    const itemObject = JSON.parse(item)
+    cart.push(itemObject )
+  }
 }
 
 function displayItem(item) {
-    const article = makeArticle(item)
-    const image = makeImage(item)
+  const article = makeArticle(item)
+  displayArticle(article)
+  console.log(article)
+  const div = makeImageDiv(item)
+  article.appendChild(div)
 }
+function displayArticle(article) {
+  document.querySelector("#cart__items").appendChild(article)
+}
+function makeArticle(item) {
+  const article = document.createElement("article")
+  article.classList.add("card__item")
+  article.dataset.id = item.id
+  article.dataset.color = item.color
+  return article
+}
+function makeImageDiv(item) {
+  const div = document.createElement("div")
+  div.classList.add("card__item__img")
 
-function makeImage(item) {
-  const image = document.createElement('img')
-  image.src = item.imageURL
+  const image = document.createElement("img")
+  image.src = item.imageUrl
   image.alt = item.altTxt
-  return image
+  div.appendChild(image)
+  return div
 }
